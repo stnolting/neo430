51c2aee326a9f5f4d97f4d8e312267885e707a41
stnolting <stnolting@gmail.com>
2018-01-06 19:44:27 +0100

    added simple test program for the default CFU

---
+++ sw/example/cfu_test/main.c
@@ -0,0 +1,113 @@
+// #################################################################################################
+// #  < Custom Functions Unit Test Program >                                                       #
+// # ********************************************************************************************* #
+// # This file is part of the NEO430 Processor project: https://github.com/stnolting/neo430        #
+// # Copyright by Stephan Nolting: stnolting@gmail.com                                             #
+// #                                                                                               #
+// # This source file may be used and distributed without restriction provided that this copyright #
+// # statement is not removed from the file and that any derivative work contains the original     #
+// # copyright notice and the associated disclaimer.                                               #
+// #                                                                                               #
+// # This source file is free software; you can redistribute it and/or modify it under the terms   #
+// # of the GNU Lesser General Public License as published by the Free Software Foundation,        #
+// # either version 3 of the License, or (at your option) any later version.                       #
+// #                                                                                               #
+// # This source is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;      #
+// # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     #
+// # See the GNU Lesser General Public License for more details.                                   #
+// #                                                                                               #
+// # You should have received a copy of the GNU Lesser General Public License along with this      #
+// # source; if not, download it from https://www.gnu.org/licenses/lgpl-3.0.en.html                #
+// # ********************************************************************************************* #
+// #  Stephan Nolting, Hannover, Germany                                               29.12.2017  #
+// #################################################################################################
+
+
+// Libraries
+#include <stdint.h>
+#include "../../lib/neo430/neo430.h"
+
+// Configuration
+#define BAUD_RATE 19200
+
+// Prototypes
+void verify16(char *s, uint16_t a, uint16_t b);
+
+
+/* ------------------------------------------------------------
+ * INFO Main function
+ * ------------------------------------------------------------ */
+int main(void) {
+
+  // setup UART
+  uart_set_baud(BAUD_RATE);
+  USI_CT = (1<<USI_CT_EN);
+
+  // intro text
+  uart_br_print("\nCustom Functions Unit (CFU) test program\n");
+
+  // check if CFU present
+  if (!(SYS_FEATURES & (1<<SYS_CFU_EN))) {
+    uart_br_print("Error! No CFU synthesized!");
+    return 1;
+  }
+
+  // wait for user to start
+  uart_br_print("Press any key to start.\n\n");
+  while (uart_char_received() == 0);
+
+  // initialize test vectors
+  uint8_t i;
+  uint16_t test_data16[16];
+  for (i=0; i<8; i++)
+    test_data16[i] = i*1728 + 913;
+
+
+  uart_br_print("Testing read/write accesses...\n");
+
+  CFU_REG0 = test_data16[0];
+  verify16("CFU_REG0", CFU_REG0, test_data16[0]);
+
+  CFU_REG1 = test_data16[1];
+  verify16("CFU_REG1", CFU_REG1, test_data16[1]);
+
+  CFU_REG2 = test_data16[2];
+  verify16("CFU_REG2", CFU_REG2, test_data16[2]);
+
+  CFU_REG3 = test_data16[3];
+  verify16("CFU_REG3", CFU_REG3, test_data16[3]);
+
+  CFU_REG4 = test_data16[4];
+  verify16("CFU_REG4", CFU_REG4, test_data16[4]);
+
+  CFU_REG5 = test_data16[5];
+  verify16("CFU_REG5", CFU_REG5, test_data16[5]);
+
+  CFU_REG6 = test_data16[6];
+  verify16("CFU_REG6", CFU_REG6, test_data16[6]);
+
+  CFU_REG7 = test_data16[7];
+  verify16("CFU_REG7", CFU_REG7, test_data16[7]);
+
+  return 0;
+}
+
+
+/* ------------------------------------------------------------
+ * INFO Verify 16-bit transfer
+ * ------------------------------------------------------------ */
+void verify16(char *s, uint16_t a, uint16_t b) {
+
+  uart_br_print("Checking ");
+  uart_br_print(s);
+  uart_br_print(" - expected: 0x");
+  uart_print_hex_word(b);
+  uart_br_print(", received: 0x");
+  uart_print_hex_word(a);
+
+  if (a == b)
+    uart_br_print(" - OK\n");
+  else
+    uart_br_print(" - FAILED\n");
+}
+
