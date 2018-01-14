8832a5a188a6be284ff2d534aad8afecb82c65de
stnolting <stnolting@gmail.com>
2018-01-14 01:24:43 +0100

    added non-blocking wishbone transfer functions

--- sw/lib/neo430/neo430_wishbone.h
+++ sw/lib/neo430/neo430_wishbone.h
@@ -20,7 +20,7 @@
 // # source; if not, download it from https://www.gnu.org/licenses/lgpl-3.0.en.html                #
 // # ********************************************************************************************* #
 // #  Thanks to Edward Sherriff!                                                                   #
-// #  Stephan Nolting, Hannover, Germany                                               27.12.2017  #
+// #  Stephan Nolting, Hannover, Germany                                               13.01.2018  #
 // #################################################################################################
 
 #ifndef neo430_wishbone_h
@@ -35,6 +35,16 @@ void wishbone_write16(uint32_t a, uint16_t d);
 
 uint8_t wishbone_read8(uint32_t a);
 void wishbone_write8(uint32_t a, uint8_t d);
+
+// prototypes non-blocking functions
+uint32_t wishbone_read32_nonblocking(uint32_t a, uint8_t* success);
+void wishbone_write32_nonblocking(uint32_t a, uint32_t d, uint8_t* success);
+
+uint16_t wishbone_read16_nonblocking(uint32_t a, uint8_t* success);
+void wishbone_write16_nonblocking(uint32_t a, uint16_t d, uint8_t* success);
+
+uint8_t wishbone_read8_nonblocking(uint32_t a, uint8_t* success);
+void wishbone_write8_nonblocking(uint32_t a, uint8_t d, uint8_t* success);
 
 
 /* ------------------------------------------------------------
@@ -170,6 +180,149 @@ void wishbone_write8(uint32_t a, uint8_t d) {
 
   // wait for access to be completed - blocking!
   while((WB32_CT & (1<<WB32_CT_PENDING)) != 0);
+}
+
+
+
+/* ------------------------------------------------------------
+ * INFO Read 32-bit from Wishbone device (non-blocking), standard mode, pipelined
+ * PARAM 32-bit device address
+ * PARAM pointer to byte variable to indicate success (=0)
+ * RETURN read data
+ * ------------------------------------------------------------ */
+uint32_t wishbone_read32_nonblocking(uint32_t a, uint8_t* success) {
+
+  // 32-bit transfer
+  WB32_CT = 0xF;
+
+  // device address aligned to 32-bit + transfer trigger
+  WB32_RA_32bit = a & (~3);
+
+  // get status - transfer successful?
+  *success = (uint8_t)(WB32_CT & (1<<WB32_CT_PENDING));
+
+  return WB32_D_32bit;
+}
+
+
+/* ------------------------------------------------------------
+ * INFO Write 32-bit to Wishbone device (non-blocking), standard mode, pipelined
+ * PARAM a: 32-bit device address
+ * PARAM d: 32-bit write data
+ * PARAM pointer to byte variable to indicate success (=0)
+ * ------------------------------------------------------------ */
+void wishbone_write32_nonblocking(uint32_t a, uint32_t d, uint8_t* success) {
+
+  // 32-bit transfer
+  WB32_CT = 0xF;
+
+  // write data
+  WB32_D_32bit = d;
+
+  // device address aligned to 32-bit + transfer trigger
+  WB32_WA_32bit = a & (~3);
+
+  // get status - transfer successful?
+  *success = (uint8_t)(WB32_CT & (1<<WB32_CT_PENDING));
+}
+
+
+/* ------------------------------------------------------------
+ * INFO Read 16-bit from Wishbone device (non-blocking), standard mode, pipelined
+ * PARAM 32-bit device address
+ * PARAM pointer to byte variable to indicate success (=0)
+ * RETURN 16-bit read data
+ * ------------------------------------------------------------ */
+uint16_t wishbone_read16_nonblocking(uint32_t a, uint8_t* success) {
+
+  // 16-bit transfer
+  if (a & 2)
+    WB32_CT = 0b1100; // high 16-bit word
+  else
+    WB32_CT = 0b0011; // low 16-bit word
+
+  // device address aligned to 16-bit + transfer trigger
+  WB32_RA_32bit = a & (~1);
+
+  // get status - transfer successful?
+  *success = (uint8_t)(WB32_CT & (1<<WB32_CT_PENDING));
+
+  if (a & 2)
+    return WB32_HD; // high 16-bit word
+  else
+    return WB32_LD; // low 16-bit word
+}
+
+
+/* ------------------------------------------------------------
+ * INFO Write 16-bit to Wishbone device (non-blocking), standard mode, pipelined
+ * PARAM a: 32-bit device address
+ * PARAM d: 16-bit write data
+ * PARAM pointer to byte variable to indicate success (=0)
+ * ------------------------------------------------------------ */
+void wishbone_write16_nonblocking(uint32_t a, uint16_t d, uint8_t* success) {
+
+  // 16-bit transfer
+  if (a & 2) {
+    WB32_CT = 0b1100; // high 16-bit word
+    WB32_HD = d;
+  }
+  else {
+    WB32_CT = 0b0011; // low 16-bit word
+    WB32_LD = d;
+  }
+
+  // get status - transfer successful?
+  *success = (uint8_t)(WB32_CT & (1<<WB32_CT_PENDING));
+
+  // wait for access to be completed - blocking!
+  while((WB32_CT & (1<<WB32_CT_PENDING)) != 0);
+}
+
+
+/* ------------------------------------------------------------
+ * INFO Read 8-bit from Wishbone device (non-blocking), standard mode, pipelined
+ * PARAM 32-bit device address
+ * PARAM pointer to byte variable to indicate success (=0)
+ * RETURN 0 if success, 1 if timeout
+ * ------------------------------------------------------------ */
+uint8_t wishbone_read8_nonblocking(uint32_t a, uint8_t* success) {
+
+  // 8-bit transfer
+  WB32_CT = 1 << (a & 3); // corresponding byte enable
+
+  // device address aligned to 8-bit + transfer trigger
+  WB32_RA_32bit = a;
+
+  // get status - transfer successful?
+  *success = (uint8_t)(WB32_CT & (1<<WB32_CT_PENDING));
+
+  // select correct byte to be written
+  volatile uint8_t* in = (uint8_t*)(&WB32_D_8bit + ((uint8_t)a & 3));
+  return *in;
+}
+
+
+/* ------------------------------------------------------------
+ * INFO Write 8-bit to Wishbone device (non-blocking), standard mode, pipelined
+ * PARAM a: 32-bit device address
+ * PARAM d: 8-bit write data
+ * PARAM pointer to byte variable to indicate success (=0)
+ * ------------------------------------------------------------ */
+void wishbone_write8_nonblocking(uint32_t a, uint8_t d, uint8_t* success) {
+
+  // 8-bit transfer
+  WB32_CT = 1 << (a & 3); // corresponding byte enable
+
+  // select correct byte to be written
+  volatile uint8_t* out = (uint8_t*)(&WB32_D_8bit + ((uint8_t)a & 3));
+  *out = d;
+
+  // device address aligned to 8-bit + transfer trigger
+  WB32_WA_32bit = a;
+
+  // get status - transfer successful?
+  *success = (uint8_t)(WB32_CT & (1<<WB32_CT_PENDING));
 }
 
 
