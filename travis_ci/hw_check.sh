a6df3a19417078247277fca7f7b56b2d5b4e3f4e
stnolting <stnolting@gmail.com>
2020-02-14 20:11:38 +0100

    corrected typo

--- travis_ci/hw_check.sh
+++ travis_ci/hw_check.sh
@@ -9,6 +9,6 @@ ls -al $srcdir_top_templates
 ls -al $srcdir_sim
 
 # Analyse sources
-ghdl -a --work=neo430 $srcdir_core/neo430_package.vhd $srcdir_core/neo430_bootloader_image.vhd $srcdir_core/neo430_application_image.vhd $srcdir_core/*.vhd $srcdir_top_templates/*.vhd $srcdir_sim/+.vhd
+ghdl -a --work=neo430 $srcdir_core/neo430_package.vhd $srcdir_core/neo430_bootloader_image.vhd $srcdir_core/neo430_application_image.vhd $srcdir_core/*.vhd $srcdir_top_templates/*.vhd $srcdir_sim/*.vhd
 
 
