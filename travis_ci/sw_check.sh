6925f1e506980e7513a93663058f4e64bf33e69f
stnolting <stnolting@gmail.com>
2020-05-14 23:44:55 +0200

    added info target to travis sw check script

--- travis_ci/sw_check.sh
+++ travis_ci/sw_check.sh
@@ -11,6 +11,9 @@ srcdir_bootloader=/mnt/data/sw/bootloader
 ls -al $srcdir_examples
 ls -al $srcdir_bootloader
 
+# check toolchain
+make -C $srcdir_examples/blink_led check
+
 # Try to compile all example projects
-make -C $srcdir_examples clean_all compile
-make -C $srcdir_bootloader clean_all all
+make -C $srcdir_examples clean_all info compile
+make -C $srcdir_bootloader clean_all info all
